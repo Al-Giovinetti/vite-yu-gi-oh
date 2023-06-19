ba00c67612478628342206f2b331e8a2713cb56f
Al-Giovinetti <giovinettialessio@gmail.com>
2023-06-19 16:50:05 +0200

    Scaricato axios e recuperata arrey dall api

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,15 +1,36 @@
 <script>
-import SingleCard from "./SingleCard.vue"
+import SingleCard from "./SingleCard.vue";
+import axios from "axios";
 
 export default{
     name:"CardList",
+
+    dat(){
+        return{
+            CardList:[ ],
+        }
+    },
+
     components:{
         SingleCard
+    },
+
+    created(){
+        axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0")
+        .then((response) => {
+            console.log(response.data.data)
+            this.CardList = response.data.data
+        })
+        .catch(function (error){
+            console.log(error)
+        })
     }
+
 }
 </script>
 
 <template>
+    {{ CardList }}
     <div class="container">
         <SingleCard />
     </div>
